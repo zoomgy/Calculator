0b790f3c13b47a7a27a0dabc44f8930a2659447f
Ayush Thakur <zoomgy@Ayushs-MacBook-Air-2.local>
2025-01-17 11:59:56 +0530

    Added Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome To Calculator");
+        int first = sc.nextInt();
+        int second = sc.nextInt();
+    }
+}
